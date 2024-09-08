7101adb2c620252ed6fbcece0dfcac8b62a2bdaf
Chichigami <g.feng.work@gmail.com>
2024-09-08 16:50:52 -0400

    updated textnode tests

--- public/src/test_textnode.py
+++ public/src/test_textnode.py
@@ -8,6 +8,23 @@ class TestTextNode(unittest.TestCase):
         node = TextNode("This is a text node", "bold")
         node2 = TextNode("This is a text node", "bold")
         self.assertEqual(node, node2)
+    
+    def test_not_eq(self):
+        node = TextNode("This is a text node", "italics", "https://www.someurl.com/")
+        node2 = TextNode("This is a text node", "bold", "https://www.someurl.com/")
+        self.assertNotEqual(node, node2)
+    
+    def test_default_url(self):
+        node = TextNode("URL is None", "bold")
+        self.assertEqual(node.url, None)
+    
+
+    def test_repr(self):
+        node = TextNode("This is a text node", "bold", "https://www.someurl.com/")
+        expected = "TextNode(This is a text node, bold, https://www.someurl.com/)"
+        actual = repr(node)
+        self.assertEqual(actual, expected)
+
 
 
 if __name__ == "__main__":
