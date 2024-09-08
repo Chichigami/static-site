785eb57bbaae44bce6b5ba98124ad74ce07d4b7d
Chichigami <g.feng.work@gmail.com>
2024-09-08 17:50:20 -0400

    added HTMLNode and testing for it

--- public/src/main.py
+++ public/src/main.py
@@ -1,9 +1,8 @@
 from textnode import *
+from htmlnode import *
 
 def main():
-    test = TextNode("This is a text node", "bold", "https://www.boot.dev")
-    print(test)
-
+    print("hello world")
 
 if __name__ == "__main__":
     main()
---
+++ public/src/test_htmlnode.py
@@ -0,0 +1,27 @@
+import unittest
+
+from htmlnode import HTMLNode
+
+class TestHTMLNode(unittest.TestCase):
+    def test_prop_None(self):
+        node = HTMLNode()
+        actual = node.props_to_html()
+        expected = None
+        self.assertEqual(actual, expected)
+    
+    def test_prop_to_html(self):
+        node = HTMLNode("tag", "value", None, {"href": "https://www.google.com", "target": "_blank",})
+        expected = 'href="https://www.google.com" target="_blank"'
+        actual = node.props_to_html()
+        self.assertEqual(actual, expected)
+    
+    def test_repr(self):
+        child = HTMLNode("a", "this has a href", None, {"href": "https://www.someurl.com/", "target": "hello world",})
+        node = HTMLNode("tag", "value", child, {"href": "https://www.google.com", "target": "_blank",})
+        expected = "HTMLNode(tag, value, HTMLNode(a, this has a href, None, {'href': 'https://www.someurl.com/', 'target': 'hello world'}), {'href': 'https://www.google.com', 'target': '_blank'})"
+        actual = repr(node)
+        self.assertEqual(actual, expected)
+    
+
+if __name__ == "__main__":
+    unittest.main()
