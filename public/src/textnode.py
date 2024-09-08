5b7d8d003681b9592d149ce1e6efe81620a457f7
Chichigami <g.feng.work@gmail.com>
2024-09-08 16:57:06 -0400

    strict type eq method

--- public/src/textnode.py
+++ public/src/textnode.py
@@ -1,11 +1,13 @@
+from typing import Self
+
 class TextNode():
     def __init__(self, text, text_type, url = None):
         self.text = text
         self.text_type = text_type #bold italics
         self.url = url #link or image
 
-    def __eq__(self, other) -> bool:
+    def __eq__(self, other: Self) -> bool:
         return self.text == other.text and self.text_type == other.text_type and self.url == other.url
 
     def __repr__(self) -> str:
-        return f"TextNode({self.text}, {self.text_type}, {self.url})"
+        return f"TextNode({self.text}, {self.text_type}, {self.url})"   
