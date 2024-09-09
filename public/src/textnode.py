f0d0ea100b40d949be1ec9cf6c39cedbf81eac9b
Chichigami <g.feng.work@gmail.com>
2024-09-09 02:31:44 -0400

    finished parentnode and testing

--- public/src/textnode.py
+++ public/src/textnode.py
@@ -1,13 +1,13 @@
 from typing import Self
 
 class TextNode():
-    def __init__(self, text, text_type, url = None):
+    def __init__(self, text: str, text_type: str, url: str = None):
         self.text = text
         self.text_type = text_type #bold italics
         self.url = url #link or image
 
     def __eq__(self, other: Self) -> bool:
-        return self.text == other.text and self.text_type == other.text_type and self.url == other.url
+        return self.text is other.text and self.text_type is other.text_type and self.url is other.url
 
     def __repr__(self) -> str:
         return f"TextNode({self.text}, {self.text_type}, {self.url})"   
