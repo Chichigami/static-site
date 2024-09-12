d966d4dd73877096117787535d1b4549896626dc
Chichigami <g.feng.work@gmail.com>
2024-09-12 17:20:38 -0400

    markdown_to_blocks finished, testing still needed. not sure if this will work fully

--- public/src/block_markdown.py
+++ public/src/block_markdown.py
@@ -4,5 +4,9 @@ from textnode import *
 from htmlnode import *
 from typing import List
 
-def markdown_to_blocks(markdown):
-    
+def markdown_to_blocks(document: str) -> List[str]:
+    splitted = document.split('\n\n')
+    blocks = []
+    for sentence in splitted:
+        blocks.append(sentence.lstrip())
+    return blocks
--- public/src/test_block_markdown.py
+++ public/src/test_block_markdown.py
@@ -12,6 +12,24 @@ class TestSplitBlockMarkdown(unittest.TestCase):
                 "* This is the first list item in a list block\n" \
                 "* This is a list item\n" \
                 "* This is another list item"
+        actual = markdown_to_blocks(text)
+        expected = ['# This is a heading',
+                    'This is a paragraph of text. It has some **bold** and *italic* words inside of it.', 
+                    '* This is the first list item in a list block\n* This is a list item\n* This is another list item']
+        self.assertEqual(actual, expected)
+
+    def test_default_with_tabs_and_spaces(self):
+        text =  "# This is a heading\n\n" \
+                " This is a paragraph of text. It has some **bold** and *italic* words inside of it.\n\n" \
+                " * This is the first list item in a list block\n" \
+                "   * This is a list item\n" \
+                "* This is another list item"
+        actual = markdown_to_blocks(text)
+        expected = ['# This is a heading',
+                    'This is a paragraph of text. It has some **bold** and *italic* words inside of it.', 
+                    '* This is the first list item in a list block\n   * This is a list item\n* This is another list item']
+        self.assertEqual(actual, expected)
+
 
 if __name__ == "__main__":
     unittest.main()
